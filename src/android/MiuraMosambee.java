8769b1c9266745d3a2c90fe7a6d488b3db77ba19
Anil <b.anil@better-life.co.in>
2016-01-15 22:19:16 +0530

    make it better

--- src/android/MiuraMosambee.java
+++ src/android/MiuraMosambee.java
@@ -5,29 +5,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.AlertDialog;
-import android.app.AlertDialog.Builder;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.os.Looper;
-import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.TextView;
-
-import com.mosambee.lib.*;
 
 public class MiuraMosambee extends CordovaPlugin {
     
-    TransactionCallback tc;
     private static final String LOG_TAG = "MiuraPlugin";
-    
-    private FrameLayout frameLayout;
-    
-    public AlertDialog.Builder dlg = null;
-    
-    private static String currentStatus = "Processing";
-    public static TextView input;
+    private FrameLayout frameLayout = null;
     private Double amount;
     private String orderid;
     private String username;
@@ -35,7 +18,7 @@ public class MiuraMosambee extends CordovaPlugin {
     
     
     @Override
-    public boolean execute(String action, final JSONArray data,final CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
         
         if (action.equals("sale")) {
             
@@ -50,7 +33,9 @@ public class MiuraMosambee extends CordovaPlugin {
                 LOG.d(LOG_TAG,"JSONException", e);
                 e.printStackTrace();
             }
-            prompt(callbackContext);
+            Result resultClass = new Result();
+            resultClass.startTransaction(amount, orderid, username, password, frameLayout,cordova,callbackContext);
+            //prompt(callbackContext);
             return true;
 
         } else {
@@ -60,76 +45,4 @@ public class MiuraMosambee extends CordovaPlugin {
         }
         
     }
-    
-    
-    
-    public void prompt(final CallbackContext callbackContext) {
-        
-        final MiuraMosambee notification = this;
-        final CordovaInterface cordova = this.cordova;
-        final Result resultClass = new Result();
-        
-        input = new TextView(cordova.getActivity().getApplicationContext());
-        
-        Runnable runnable = new Runnable() {
-            public void run() {
-                //AlertDialog.Builder dlg = createDialog(cordova); // new AlertDialog.Builder(cordova.getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
-                notification.dlg = createDialog(cordova);
-                notification.dlg.setMessage(currentStatus);
-                notification.dlg.setTitle("Capture Payment");
-                notification.dlg.setCancelable(false);
-                notification.dlg.setView(input);
-                
-                notification.dlg.setNeutralButton("Abort",
-                        new AlertDialog.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Aborted"));
-                            }
-                    });
-            
-                notification.dlg.show();
-                
-                Runnable childRunnable = new Runnable(){
-                    public void run(){
-                        Looper.prepare();
-                        resultClass.startTransaction(amount, orderid, username, password, frameLayout,cordova);
-                        Looper.loop();
-                    }
-                };
-                
-                cordova.getThreadPool().execute(childRunnable);
-                
-            };
-        };
-        this.cordova.getActivity().runOnUiThread(runnable);
-    }
-    
-    
-    public static void setData(ResultData result) {
-        
-        String text = "";
-        if (result== null) {
-            text = "NULL";
-        } else {
-            text =  "Result: " + result.getResult() + "\nReason code: "
-                    + result.getReasonCode() + "\nReason: "
-                    + result.getReason() + "\nTranaction Id: "
-                    + result.getTransactionId() + "\nTransactin amount: "+
-                    result.getAmount()+"\nTransactin data: "
-                    + result.getTransactionData();
-        }
-        input.setText(text);
-    }
-    
-    
-    private AlertDialog.Builder createDialog(CordovaInterface cordova) {
-        return new AlertDialog.Builder(cordova.getActivity());
-    }
-
-   private ProgressDialog createProgressDialog(CordovaInterface cordova) {
-        
-        return new ProgressDialog(cordova.getActivity());
-    }
-   
 }
--- src/android/Result.java
+++ src/android/Result.java
@@ -3,30 +3,182 @@ import com.mosambee.lib.*;
 
 import org.apache.cordova.*;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
+import android.os.Looper;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import org.apache.cordova.PluginResult;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Result implements TransactionResult {
 
-	private Context context;
-	TransactionCallback tc;
+	private static final String LOG_TAG = "MiuraPlugin-Command";
 	
-	void startTransaction(Double amount,String orderid,String username,String password,FrameLayout frameLayout, CordovaInterface cordova){
+	private JSONObject resultObj;
+	private TextView progress;
+	private AlertDialog.Builder dlg = null;
+	private Context context;
+	private TransactionCallback tc;
+	private CallbackContext callbackContext;
+	private CordovaInterface cordova;
+	
+	
+	void startTransaction(Double amount,String orderid,String username,String password,FrameLayout frameLayout, CordovaInterface cordova, CallbackContext callbackContext){
+		this.callbackContext = callbackContext;
+		this.cordova = cordova;
+		progress = new TextView(cordova.getActivity().getApplicationContext());
+    	resultObj = new JSONObject();
 		setContext(cordova.getActivity().getApplicationContext());
+		prompt(username,password,orderid,amount);
+		
+	}
+	
+	public void prompt(final String username,final String password,final String orderid, final Double amount ) {
+      	
+		final Result notification = this;
+		
+    	Runnable runnable = new Runnable() {
+    		public void run() {
+		    	
+    			/*
+    			notification.dlg = createDialog(cordova);
+		    	notification.dlg.setMessage("Processing");
+		    	notification.dlg.setTitle("Capture Payment");
+		    	notification.dlg.setCancelable(false);
+		    	notification.dlg.setView(progress);
+		        
+		    	notification.dlg.setNeutralButton("Close",
+		    			new AlertDialog.OnClickListener() {
+		            		public void onClick(DialogInterface dialog, int which) {
+		            			dialog.dismiss();
+		            			callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, resultObj));
+		            		}
+		        	});
+	    	
+	    		notification.dlg.show();
+	    		
+	    		notification.dlg.setOnDismissListener(new OnDismissListener() {
+	    			public void onDismiss(final DialogInterface dialog) {
+	    		        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, resultObj));
+	    		    }
+	    		});
+	    		*/
+	    		
+	    		Runnable childRunnable = new Runnable(){
+	    			public void run(){
+	    				Looper.prepare();
+	    				processTransaction(username,password,orderid,amount);
+	    				Looper.loop();
+	    			}
+	    		};
+	    		cordova.getThreadPool().execute(childRunnable);
+	    		
+    			//processTransaction(username,password,orderid,amount);
+	    		
+            };
+        };
+        cordova.getActivity().runOnUiThread(runnable);
+    }
+	
+	private void showDialog(){
+		
+		this.dlg = createDialog(cordova);
+    	this.dlg.setMessage("Processing");
+    	this.dlg.setTitle("Capture Payment");
+    	this.dlg.setCancelable(false);
+    	this.dlg.setView(progress);
+    	
+    	this.dlg.setNeutralButton("Close",
+    			new AlertDialog.OnClickListener() {
+            		public void onClick(DialogInterface dialog, int which) {
+            			dialog.dismiss();
+            			callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, resultObj));
+            		}
+        	});
+	
+		this.dlg.show();
+		
+		this.dlg.setOnDismissListener(new OnDismissListener() {
+			public void onDismiss(final DialogInterface dialog) {
+		        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, resultObj));
+		    }
+		});
+	}
+	
+	private void processTransaction(String username,String password,String orderid,Double amount){
 		tc = new MosCallback(context);
-		tc.initialise(username,password , this);
+		tc.initialise(username,password,this);
 		//tc.initializeSignatureView(frameLayout, "#55004A", "#750F5A");
 		tc.processTransaction(orderid, amount, null, null);
 	}
-	
+
 	@Override
 	public void onResult(ResultData resultData) {
-		MiuraMosambee.setData(resultData);
+		
+		boolean success = false;
+    	String reasonCode = "";
+    	String reason = "";
+    	String transactionId = "";
+    	String amount = "";
+    	String transactionData = "";
+    	
+    	String text = "";
+		if (resultData== null) {
+			text = "NULL";
+		} else {
+			success = resultData.getResult();
+			reasonCode = resultData.getReasonCode();
+			reason = resultData.getReason();
+			transactionId = resultData.getTransactionId();
+			amount = resultData.getAmount();
+			transactionData = resultData.getTransactionData();
+			
+			text =  "Result: " + success + "\nReason code: "
+					+ reasonCode + "\nReason: "
+					+ reason + "\nTranaction Id: "
+					+ transactionId + "\nTransactin amount: "
+					+ amount +"\nTransactin data: "
+					+ transactionData;
+		}
+		try {
+			resultObj.put("success", success);
+			resultObj.put("reasonCode", reasonCode);
+			resultObj.put("reason", reason);
+			resultObj.put("transactionId", transactionId);
+			resultObj.put("amount", amount);
+			resultObj.put("transactionData", transactionData);
+		} catch (JSONException e) {
+			e.printStackTrace();
+			LOG.d(LOG_TAG,"JSONException result", e);
+		}
+		callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, resultObj));
+		
 	}
 	
 	public void setContext(Context context) {
 		this.context = context;
 	}
+
+	@Override
+	public void onCommand(String arg0) {
+		LOG.d(LOG_TAG,arg0);
+		try {
+			resultObj.put("reason", arg0);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		PluginResult res = new PluginResult(PluginResult.Status.OK, resultObj);
+		res.setKeepCallback(true);
+		callbackContext.sendPluginResult(res);
+		progress.setText(arg0);
+	}
+	
+	private AlertDialog.Builder createDialog(CordovaInterface cordova) {
+        return new AlertDialog.Builder(cordova.getActivity());
+    }
 }
